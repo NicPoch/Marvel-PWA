c743584365905c9a334a1c21aa2542e1baf2782d
Nicolas Poch <n.poch@uniandes.edu.co>
2021-05-11 22:01:18 -0500

    final working pwa

--- src/components/characterDetail.js
+++ src/components/characterDetail.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Card,Row,Col} from "react-bootstrap";
 
 const CharacterDetail=(props)=>{
+    const [state, setstate] = useState({info:props.info});
 
+    const renderDescription=()=>{
+        if(state.info.description==="")
+        {
+            return(
+                <p>
+                   No Description Available
+                </p>
+                );    
+        }
+        else
+        {
+            return(
+                <p>
+                   {state.info.description}
+                </p>
+                );    
+        }
+    };
+
+    return(
+        <Card.Body>
+            <Row>                
+                <Col>
+                    <h3>{state.info.name}</h3>
+                </Col>
+                <Col>
+                    <h3>ID:{state.info.id}</h3>
+                </Col>
+            </Row>
+            <Row>
+                <h4>Description</h4>
+                {renderDescription()}
+            </Row>            
+        </Card.Body>
+    );
 };
 
 export default CharacterDetail;
--- src/components/characterList.js
+++ src/components/characterList.js
@@ -1,15 +1,59 @@
 import React,{useState,useEffect} from 'react';
-
-const url="http://gateway.marvel.com/v1/public/characters?limit=100ts=1&apikey=07ebaf36d95728b8183def7844d73f65&hash=a3f3703281020deb2224463dda326ab2"; 
+import {Accordion, Card} from "react-bootstrap";
+import CharacterDetail from './characterDetail';
 
 const CharacterList=()=>{
+    const url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=07ebaf36d95728b8183def7844d73f65&hash=a3f3703281020deb2224463dda326ab2"; 
     const [state, setstate] = useState({characters:[]});
     useEffect(() => {
-        fetch(url).then(res=>res.json()).then(ans=>console.log(ans)).catch(err=>console.log(err));
+        fetch(url).then(res=>res.json()).then(ans=>{
+            if(!navigator.onLine)
+            {
+                let info=localStorage.getItem("characters");
+                if(info !== null)
+                {
+                    setstate({characters:info});
+                }
+            }
+            else
+            {
+                localStorage.setItem("characters",JSON.stringify(ans.data.results));
+                setstate({characters:ans.data.results});
+            }
+        }).catch(err=>console.log(err));
     }, []);
 
+    const render=()=>{
+        if(state.characters.length===0)
+        {
+            return(<h2>Loading ... :P</h2>);
+        }
+        else
+        {
+            return(
+                <Accordion>
+                    {state.characters.map(c=>(
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Card.Header} variant="link" eventKey={c.id}>
+                                    {c.name}
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey={c.id}>
+                                <CharacterDetail info={c} key={c.id}/>
+                            </Accordion.Collapse>
+                        </Card>                    
+                    ))}
+                </Accordion>
+            );
+        }
+    };
+
     return(
-        <h2>Character List</h2>
+        <div>
+            <h2>Character List</h2>
+            {render()}           
+        </div>        
     );
 };
 
